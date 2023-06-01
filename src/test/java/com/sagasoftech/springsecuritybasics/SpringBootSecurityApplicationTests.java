47751217ae3353874b79a0c337f9ec3ce602fccf
Sagar Dighe <sagasoftech@gmail.com>
2023-06-01 00:51:14 -0500

    Initial setup for Spring Security Demo

---
+++ src/test/java/com/sagasoftech/springsecuritybasics/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
