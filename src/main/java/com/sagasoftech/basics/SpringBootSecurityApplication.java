33f299ae98b45e489cb6f70d95e933142029ab8e
Sagar Dighe <sagasoftech@gmail.com>
2023-06-01 01:01:56 -0500

    Add package for Component scan

--- src/main/java/com/sagasoftech/basics/SpringBootSecurityApplication.java
+++ src/main/java/com/sagasoftech/basics/SpringBootSecurityApplication.java
@@ -2,8 +2,15 @@ package com.sagasoftech.basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+/*
+ * This is optional for this project as the package to scan in inside our main package 
+ * where spring boot application main class is present
+ * However, it required if controller class are outside of the package of spring boot main class
+ */
+@ComponentScan("com.sagasoftech.basics.controller")
 public class SpringBootSecurityApplication {
 
 	public static void main(String[] args) {
