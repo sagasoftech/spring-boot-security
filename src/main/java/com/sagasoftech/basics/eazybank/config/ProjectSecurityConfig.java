cf4b116ad89f050a10c7d165246a8e1c34ffb19d
Sagar Dighe <sagasoftech@gmail.com>
2023-06-04 18:21:36 -0500

    Deny all the requests coming towards web applicaion APIs: First it will Authenticate the request credentials entered. Then after successfully authenticating, it will return 403 error which is denied Authorization error

--- src/main/java/com/sagasoftech/basics/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/sagasoftech/basics/eazybank/config/ProjectSecurityConfig.java
@@ -13,9 +13,7 @@ public class ProjectSecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests
-			.requestMatchers("/myAccount", "/myBalance","/myLoans","/myCards").authenticated()
-			.requestMatchers("/notices","/contact").permitAll());
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		
