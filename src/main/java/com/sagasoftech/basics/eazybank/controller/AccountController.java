30a03f99272047d797f0e4c27b25d1be96201a36
Sagar Dighe <sagasoftech@gmail.com>
2023-07-16 23:00:50 -0500

    Use email id as input param: Because, third party appliction will not know internal ids. So better to use email

--- src/main/java/com/sagasoftech/basics/eazybank/controller/AccountController.java
+++ src/main/java/com/sagasoftech/basics/eazybank/controller/AccountController.java
@@ -1,7 +1,12 @@
 package com.sagasoftech.basics.eazybank.controller;
 
 import com.sagasoftech.basics.eazybank.model.Accounts;
+import com.sagasoftech.basics.eazybank.model.Customer;
 import com.sagasoftech.basics.eazybank.repository.AccountsRepository;
+import com.sagasoftech.basics.eazybank.repository.CustomerRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +17,20 @@ public class AccountController {
 
     @Autowired
     private AccountsRepository accountsRepository;
+    
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @GetMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestParam int id) {
-        Accounts accounts = accountsRepository.findByCustomerId(id);
-        if (accounts != null ) {
-            return accounts;
-        }else {
-            return null;
+    public Accounts getAccountDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            Accounts accounts = accountsRepository.findByCustomerId(customers.get(0).getId());
+            if (accounts != null) {
+                return accounts;
+            }
         }
+        return null;
     }
 
 }
--- src/main/java/com/sagasoftech/basics/eazybank/controller/BalanceController.java
+++ src/main/java/com/sagasoftech/basics/eazybank/controller/BalanceController.java
@@ -1,7 +1,10 @@
 package com.sagasoftech.basics.eazybank.controller;
 
 import com.sagasoftech.basics.eazybank.model.AccountTransactions;
+import com.sagasoftech.basics.eazybank.model.Customer;
 import com.sagasoftech.basics.eazybank.repository.AccountTransactionsRepository;
+import com.sagasoftech.basics.eazybank.repository.CustomerRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +18,19 @@ public class BalanceController {
     @Autowired
     private AccountTransactionsRepository accountTransactionsRepository;
 
+    @Autowired
+    private CustomerRepository customerRepository;
+	
     @GetMapping("/myBalance")
-    public List<AccountTransactions> getBalanceDetails(@RequestParam int id) {
-        List<AccountTransactions> accountTransactions = accountTransactionsRepository.
-                findByCustomerIdOrderByTransactionDtDesc(id);
-        if (accountTransactions != null ) {
-            return accountTransactions;
-        }else {
-            return null;
+    public List<AccountTransactions> getBalanceDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                    findByCustomerIdOrderByTransactionDtDesc(customers.get(0).getId());
+            if (accountTransactions != null ) {
+                return accountTransactions;
+            }
         }
+        return null;
     }
 }
